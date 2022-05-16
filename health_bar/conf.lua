5c4d4de7616bdc6397913c6b8f8451363ca6d33c
Luis Martinez <luisho@Luiss-iMac.local>
2022-05-16 11:53:31 -0300

    First commit

---
+++ health_bar/conf.lua
@@ -0,0 +1,7 @@
+function love.conf(t)
+    t.title = 'Health Bar'
+    t.version = '11.4'
+    t.console = true
+    t.window.width = 640
+    t.window.height = 576
+end
---
+++ health_bar/main.lua
@@ -0,0 +1,94 @@
+function love.load()
+    healthbar = {}
+    healthbar.increaser = 4
+    healthbar.x = 32 * healthbar.increaser
+    healthbar.y = 47 * healthbar.increaser
+    healthbar.width = 92 * healthbar.increaser
+    healthbar.height = 12 * healthbar.increaser
+    healthbar.life = 4
+    healthbar.background = love.graphics.newImage('sprites/background.png') 
+    healthbar.bar = love.graphics.newImage('sprites/bar.png') 
+
+    life = {}
+    life.recovery = 1
+    life.damage = 1
+
+    font = love.graphics.newFont('font/Minecraft.ttf', 16 * healthbar.increaser, 'normal', 16)
+end
+
+function love.update(dt)
+   -- nothing here :P
+end
+
+function love.draw()
+    -- background
+    love.graphics.draw(healthbar.background, 0, 0)
+    
+    -- health bar
+    love.graphics.draw(healthbar.bar, healthbar.x, healthbar.y)
+
+    -- health bar status
+    healthbarStatus()
+    love.graphics.rectangle ('fill', healthbar.x + 8, healthbar.y + 8, healthbar.width, healthbar.height)
+
+    -- print title
+    love.graphics.setColor(29 / 255, 43 / 255, 83 / 255, 1) -- color dark blue
+    love.graphics.setFont(font)
+    love.graphics.print('HEALTH BAR', 29 * healthbar.increaser, 20 * healthbar.increaser)
+
+    -- print health values
+    healthValues()
+
+    -- reset color
+    love.graphics.setColor(1, 1, 1, 1)
+    
+end
+
+----------------------
+-- CUSTOM FUNCTIONS --
+----------------------
+
+-- healthbar status
+function healthbarStatus()
+    -- health bar behaviour
+    if healthbar.life >= 4 then 
+        -- full green bar
+        love.graphics.setColor(0, 228 / 255, 54 / 255, 1) -- color green
+        healthbar.width = 368 -- 100%
+    elseif healthbar.life == 3 then
+        -- green bar
+        love.graphics.setColor(41 / 255, 173 / 255, 1, 1) -- color blue
+        healthbar.width = 276 -- 75%
+    elseif healthbar.life == 2 then
+        -- yellow bar
+        love.graphics.setColor(1, 236 / 255, 39 / 255, 1) -- color yellow
+        healthbar.width = 184 -- 50%
+    elseif healthbar.life == 1 then
+        -- red bar
+        love.graphics.setColor(1, 0, 77 / 255, 1) -- color red
+        healthbar.width = 92 -- 25%
+    elseif healthbar.life == 0 then
+        -- no bar
+        healthbar.width = 0 -- 0%
+    end
+end
+
+-- health bar values
+function healthValues()
+    love.graphics.setColor(29 / 255, 43 / 255, 83 / 255, 1) -- color dark blue
+    if healthbar.life >= 1 and healthbar.life <= 4 then 
+        love.graphics.print(healthbar.life, 76 * healthbar.increaser, 74 * healthbar.increaser)
+    elseif healthbar.life == 0 then
+        love.graphics.print('DEAD', 59 * healthbar.increaser, 74 * healthbar.increaser)
+    end
+end
+
+-- check life on the healthbar
+function love.keypressed(key)
+    if key == 'up' and healthbar.life >= 0 and healthbar.life <= 3 then
+        healthbar.life = healthbar.life + life.recovery
+    elseif key == 'down' and healthbar.life >= 1 and healthbar.life <= 4 then
+        healthbar.life = healthbar.life - life.damage
+    end
+end
+
